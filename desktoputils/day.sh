d1f4ddf2628cabf08a4201cca5f8bf7ef91db7f2
arnauKL <arnaukdeprez@gmail.com>
2026-06-21 11:37:06 +0200

    updates

--- desktoputils/day.sh
+++ desktoputils/day.sh
@@ -2,17 +2,22 @@
 
 # configured to work on arch + labwc
 
-# bg (swaybg)
-#pkill swaybg; swaybg -i "$(cat /var/tmp/selected-bg)" -m fill &
-#pkill swaybg && swaybg -c "#e0e0e0" &
-
 gsettings set org.gnome.desktop.interface color-scheme "prefer-light"
 gsettings set org.gnome.desktop.interface gtk-theme "Adwaita"
+gsettings set org.gnome.desktop.interface gtk-application-prefer-dark-theme false
 
 # foot
 CONFIG="${XDG_CONFIG_HOME:-$HOME/.config}/foot/foot.ini"
-sed -i 's/initial-color-theme=[0-9]*/initial-color-theme=2/' "$CONFIG"
+sed -i 's/initial-color-theme=dark*/initial-color-theme=light/' "$CONFIG"
 
 pkill -u "$USER" --signal=SIGUSR2 ^foot$
+
+# clement bar and borders
+rm ~/.config/clement/colors
+ln ~/.config/clement/yellow_lightcolors ~/.config/clement/colors
+
+# kde apps
+kwriteconfig6 --file kdeglobals --group General --key ColorScheme BreezeLight
+kwriteconfig6 --file kdeglobals --group KDE --key LookAndFeelPackage org.kde.breezelight.desktop
 
 notify-send "Light mode"
--- desktoputils/night.sh
+++ desktoputils/night.sh
@@ -2,20 +2,22 @@
 
 # configured to work on arch + labwc
 
-# bg (swaybg)
-#pkill swaybg; swaybg -i "$(cat /var/tmp/selected-bg)" -m fill &
-#pkill swaybg; swaybg -c "#5a5a5a" & # neutral gray-ish
-#pkill swaybg && swaybg -c "#3c3836" &
-
 gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
 gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
-
-#pgrep -x waybar && pkill waybar && waybar &
+gsettings set org.gnome.desktop.interface gtk-application-prefer-dark-theme true
 
 # foot
 CONFIG="${XDG_CONFIG_HOME:-$HOME/.config}/foot/foot.ini"
-sed -i 's/initial-color-theme=[0-9]*/initial-color-theme=1/' "$CONFIG"
+sed -i 's/initial-color-theme=light*/initial-color-theme=dark/' "$CONFIG"
 
 pkill -u "$USER" --signal=SIGUSR1 ^foot$
+
+# clement bar and border colors:
+rm ~/.config/clement/colors
+ln ~/.config/clement/yellow_darkcolors ~/.config/clement/colors
+
+# kde apps
+kwriteconfig6 --file kdeglobals --group General --key ColorScheme BreezeDark
+kwriteconfig6 --file kdeglobals --group KDE --key LookAndFeelPackage org.kde.breezedark.desktop
 
 notify-send "Dark mode"
